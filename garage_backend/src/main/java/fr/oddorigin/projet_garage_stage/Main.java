ece0786a65dd06da28ff3dec4dc81345adeb9d84
OddOrigin <julien.turcot@icloud.com>
2024-03-12 13:00:55 +0100

    First CRUD test

--- garage_backend/src/main/java/fr/oddorigin/projet_garage_stage/Main.java
+++ garage_backend/src/main/java/fr/oddorigin/projet_garage_stage/Main.java
@@ -6,6 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNullApi;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.stream.Stream;
 
@@ -23,6 +26,16 @@ public class Main {
                 userRepository.save(user);
             });
             userRepository.findAll().forEach(System.out::println);
+        };
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
         };
     }
 }
--- garage_frontend/src/app/app.module.ts
+++ garage_frontend/src/app/app.module.ts
@@ -1,28 +1,28 @@
-import {NgModule} from "@angular/core";
-import {AppComponent} from "./components/app/app.component";
 import {UserListComponent} from "./components/user_list/user-list.component";
-import {AppRoutingModule} from "./app-routing.module";
 import {BrowserModule} from "@angular/platform-browser";
 import {HttpClientModule} from "@angular/common/http";
 import {UserService} from "./model/user.service";
 import {UserFormComponent} from "./components/user_form/user-form.component";
 import {FormsModule} from "@angular/forms";
+import {AppRoutingModule} from "./app-routing.module";
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./components/app/app.component";
 
 @NgModule({
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        AppRoutingModule,
+    ],
+    bootstrap: [AppComponent],
+    providers: [
+        UserService,
+    ],
     declarations: [
         AppComponent,
         UserListComponent,
         UserFormComponent,
     ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        AppRoutingModule,
-        FormsModule,
-    ],
-    providers: [
-        UserService,
-    ],
-    bootstrap: [AppComponent],
 })
 export class AppModule {}
